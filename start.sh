c83ca2c2a6dc2f437b9b89d58b88a3424267f557
Juan Sueldo <78236717+juansueldo@users.noreply.github.com>
2025-09-25 13:02:34 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 set -e
 
-# Limpiar caches de Laravel
+# Optimización y permisos Laravel
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 
-# Asegurarse de que los permisos estén correctos
 chown -R www-data:www-data /var/www/html
 chmod -R 755 /var/www/html/storage
 chmod -R 755 /var/www/html/bootstrap/cache
 
-# Iniciar Supervisor en primer plano
+# Iniciar Supervisord
 exec /usr/bin/supervisord -n -c /etc/supervisor/conf.d/supervisord.conf
